b882ca831f8430c4bd6ddf6b29ee64d4c2e503cf
JackieWeckie <nekit4568i@gmail.com>
2025-07-19 18:30:20 +0300

    initial commit

---
+++ src/main/java/com/homework18072025/Homework18072025Application.java
@@ -0,0 +1,13 @@
+package com.homework18072025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework18072025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework18072025Application.class, args);
+    }
+
+}
